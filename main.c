7369741868e7f297a4f01e85e5fed6624a925b0d
beyondStrength <iuri.turano@gmail.com>
2022-02-23 10:32:42 -0300

    Hello World

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "sudoku.h"
+
+int main(void)
+{
+  ft_putstr("Hello World!\n");
+  return (0); 
+}
---
+++ sudoku.h
@@ -0,0 +1,7 @@
+#ifndef SUDOKU
+#define SUDOKU
+
+void  ft_putchar(char c);
+void  ft_putstr(char *str);  
+
+#endif
